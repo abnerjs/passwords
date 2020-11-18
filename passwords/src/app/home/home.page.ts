34505e7e244dd5a595afde1716cc4b4521e8dd5e
abnerjs <abner.js05@gmail.com>
2020-11-18 18:12:01 -0300

    home initialize variables and searchbar

--- passwords/src/app/home/home.page.ts
+++ passwords/src/app/home/home.page.ts
@@ -1,4 +1,12 @@
 import { Component } from '@angular/core';
+import { StorageService } from '../service/storage.service';
+
+interface Senha {
+  id: number;
+  nome: string;
+  login: string;
+  senha: string;
+}
 
 @Component({
   selector: 'app-home',
@@ -6,7 +14,31 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  public senha: Senha = {} as any;
+  public senhas: Senha[] = [];
 
-  constructor() {}
+  constructor(private storage: StorageService) {
+    this.storage.recuperar('senhas').then((data: any) => {
+      if (data != null) {
+        this.senhas = data;
+      } else {
+        this.senhas = [];
+      }
+    });
+  }
 
+  adicionar() {
+    this.senha.id = new Date().getTime();
+    this.senhas.push(this.senha);
+    console.log(this.senhas);
+    this.senha = {} as any;
+
+    this.storage.armazenar('senhas', this.senhas);
+  }
+
+  excluir(id: number) {
+    this.senhas = this.senhas.filter((item) => {
+      return item.id !== id;
+    });
+  }
 }
